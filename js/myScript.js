7efbc6d6e8af6cb6ac5961ce7eff2fe636a6f6ad
mariajose0724 <58195723+mariajose0724@users.noreply.github.com>
2019-11-25 19:49:43 -0500

    Add files via upload

---
+++ js/myScript.js
@@ -0,0 +1,13 @@
+function temperatureConverter(valNum) {
+  valNum = parseFloat(valNum);
+  //The parseFloat() function parses a string
+  //and returns a floating point number.
+  document.getElementById("outputCelsius").innerHTML=(valNum-32)/1.8;
+}
+
+function calculateSquare(valNum) {
+  valNum = parseFloat(valNum);
+  //The parseFloat() function parses a string
+  //and returns a floating point number.
+  document.getElementById("outputCelsius").innerHTML=valNum*valNum;
+}
